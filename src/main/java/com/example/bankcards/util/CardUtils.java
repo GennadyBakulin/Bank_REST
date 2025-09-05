1f2d8ddb4eca83ea14a3257251aa997d249782c3
gennady bakulin <g.bakulin@yandex.ru>
2025-09-05 19:30:59 +0700

    create documentation Utils

--- src/main/java/com/example/bankcards/util/CardUtils.java
+++ src/main/java/com/example/bankcards/util/CardUtils.java
@@ -1,11 +1,30 @@
 package com.example.bankcards.util;
 
+/**
+ * Утилитарный класс для работы с банковскими картами.
+ * Содержит методы для валидации и маскировки номеров карт.
+ */
 public final class CardUtils {
 
+    /**
+     * Проверяет валидность номера банковской карты.
+     * Номер карты должен состоять ровно из 16 цифр.
+     *
+     * @param cardNumber номер карты для проверки
+     * @return true если номер карты валиден, иначе false
+     */
     public static boolean validateCardNumber(String cardNumber) {
         return cardNumber.matches("[0-9]{16}");
     }
 
+    /**
+     * Маскирует номер банковской карты для безопасного отображения.
+     * Заменяет первые 12 цифр на звездочки, оставляя видимыми последние 4 цифры.
+     * Формат маскировки: "**** **** **** XXXX", где XXXX - последние 4 цифры номера карты.
+     *
+     * @param cardNumber номер карты для маскировки
+     * @return маскированный номер карты в формате "**** **** **** XXXX"
+     */
     public static String getMaskedCardNumber(String cardNumber) {
         return "**** **** **** " + cardNumber.substring(12);
     }
--- src/main/java/com/example/bankcards/util/UserUtils.java
+++ src/main/java/com/example/bankcards/util/UserUtils.java
@@ -2,12 +2,29 @@ package com.example.bankcards.util;
 
 import com.example.bankcards.entity.user.User;
 
+/**
+ * Утилитарный класс для работы с пользователями.
+ * Содержит методы для создания полного имени пользователей и валидации паролей.
+ */
 public final class UserUtils {
 
+    /**
+     * Формирует полное имя пользователя из имени и фамилии.
+     *
+     * @param user объект пользователя
+     * @return строка с полным именем в формате "Имя Фамилия"
+     */
     public static String getFullName(User user) {
         return user.getName() + " " + user.getLastName();
     }
 
+    /**
+     * Проверяет валидность пароля пользователя.
+     * Пароль должен содержать от 5 до 16 символов, включая цифры и буквы (латинские) в верхнем и нижнем регистре.
+     *
+     * @param password пароль для проверки
+     * @return true если пароль соответствует требованиям, иначе false
+     */
     public static boolean isValidPassword(String password) {
         return password.matches("[0-9a-zA-Z]{5,16}");
     }
