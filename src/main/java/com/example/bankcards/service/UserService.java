b4d824f8714776c7c0ba00be931765e68d2c4678
gennady bakulin <g.bakulin@yandex.ru>
2025-09-05 18:54:31 +0700

    create documentation UserService

--- src/main/java/com/example/bankcards/service/UserService.java
+++ src/main/java/com/example/bankcards/service/UserService.java
@@ -3,20 +3,37 @@ package com.example.bankcards.service;
 import com.example.bankcards.dto.page.PageDtoResponse;
 import com.example.bankcards.dto.user.UserDtoResponse;
 
+/**
+ * Сервис для управления пользователями.
+ */
 public interface UserService {
 
     /**
-     * Администратор получает пользователя по email
+     * Получает информацию о пользователе по email.
+     * Метод доступен только для администраторов.
+     *
+     * @param email email пользователя для поиска
+     * @return UserDtoResponse с информацией о пользователе
+     * @throws com.example.bankcards.exception.exceptions.ResourceNotFoundException если пользователь с указанным email не найден
      */
     UserDtoResponse getUserByEmail(String email);
 
     /**
-     * Администратор получает всех пользователей с пагинацией
+     * Получает список всех пользователей с пагинацией.
+     * Метод доступен только для администраторов.
+     *
+     * @param pageNumber номер страницы (начинается с 0)
+     * @param pageSize   количество пользователей на странице
+     * @return PageDtoResponse<UserDtoResponse> страница с пользователями
      */
     PageDtoResponse<UserDtoResponse> getAllUsers(int pageNumber, int pageSize);
 
     /**
-     * Администратор удаляет пользователя по email
+     * Удаляет пользователя по email.
+     * Метод доступен только для администраторов.
+     *
+     * @param email email пользователя для удаления
+     * @throws com.example.bankcards.exception.exceptions.ResourceNotFoundException если пользователь с указанным email не найден
      */
     void deleteByEmail(String email);
 }
--- src/main/java/com/example/bankcards/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/bankcards/service/impl/UserServiceImpl.java
@@ -14,17 +14,36 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+/**
+ * Реализация сервиса для управления пользователями.
+ */
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
+    /**
+     * Получает информацию о пользователе по email.
+     * Метод доступен только для администраторов.
+     *
+     * @param email email пользователя для поиска
+     * @return UserDtoResponse с информацией о пользователе
+     * @throws com.example.bankcards.exception.exceptions.ResourceNotFoundException если пользователь с указанным email не найден
+     */
     @Override
     public UserDtoResponse getUserByEmail(String email) {
         return mapperToDto(findUserByEmail(email));
     }
 
+    /**
+     * Получает список всех пользователей с пагинацией.
+     * Метод доступен только для администраторов.
+     *
+     * @param pageNumber номер страницы (начинается с 0)
+     * @param pageSize   количество пользователей на странице
+     * @return PageDtoResponse<UserDtoResponse> страница с пользователями
+     */
     @Override
     public PageDtoResponse<UserDtoResponse> getAllUsers(int pageNumber, int pageSize) {
         Page<User> pageUsers = userRepository.findAll(PageRequest.of(pageNumber, pageSize));
@@ -40,6 +59,13 @@ public class UserServiceImpl implements UserService {
                 pageUsers.getNumber());
     }
 
+    /**
+     * Удаляет пользователя по email.
+     * Метод доступен только для администраторов.
+     *
+     * @param email email пользователя для удаления
+     * @throws com.example.bankcards.exception.exceptions.ResourceNotFoundException если пользователь с указанным email не найден
+     */
     @Override
     @Transactional
     public void deleteByEmail(String email) {
@@ -52,6 +78,12 @@ public class UserServiceImpl implements UserService {
                 .orElseThrow(() -> new ResourceNotFoundException("User with email= " + email + " not found"));
     }
 
+    /**
+     * Преобразует сущность User в DTO объект UserDtoResponse.
+     *
+     * @param user сущность пользователя
+     * @return UserDtoResponse с данными пользователя
+     */
     private UserDtoResponse mapperToDto(User user) {
         return UserDtoResponse.builder()
                 .email(user.getEmail())
